fa0d1e99f49f72c48cfba9bd67c0a182174020ed
Ashutosh Sathe <2ashutoshbs@gmail.com>
2018-12-22 12:36:54 +0530

    UI updates

--- app/src/main/java/com/absathe/byway/MapsActivity.java
+++ app/src/main/java/com/absathe/byway/MapsActivity.java
@@ -1,22 +1,34 @@
 package com.absathe.byway;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.google.android.gms.common.logging.Logger;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -26,14 +38,13 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.List;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private SupportMapFragment mapFragment;
@@ -45,12 +56,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        setSupportActionBar((Toolbar)findViewById(R.id.mapsactivity_bottomappbar));
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    /*
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_maps, container, false);
+
+        return rootView;
+    }
+    */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mapsactivity_bottomappbar_menu, menu);
+        Toast.makeText(this, "Inflated Menu", Toast.LENGTH_LONG).show();
+        super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
     @Override
     public void onPause() {
         super.onPause();
@@ -74,15 +105,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LatLng shaniwarwada = new LatLng(18.5194589,73.8531296);
+        mMap.addMarker(new MarkerOptions()
+                            .position(shaniwarwada)
+                            .icon(vectorToBitmap(R.drawable.ic_navigation, Color.parseColor("#3F51B5")))
+                            .title("You are here")
+        );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(shaniwarwada, 16));
+        /**
+         * Don't make continuous location request for the sake of demo
+         */
+        /*
         mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(5000);
         mLocationRequest.setFastestInterval(5000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-
+        Log.d("MapsActivity:", "\n----------------------------\nI'm in onMapReady Callback\n----------------------------");
         if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if(ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+                    Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                 mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback,
                         Looper.myLooper());
             }
@@ -90,6 +132,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 checkLocationPermission();
             }
         }
+        */
         // Add a marker in Sydney and move the camera
         /*
         LatLng sydney = new LatLng(-34, 151);
@@ -97,6 +140,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
         */
+    }
+    private BitmapDescriptor vectorToBitmap(@DrawableRes int id, @ColorInt int color) {
+        Drawable vectorDrawable = ResourcesCompat.getDrawable(getResources(), id, null);
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        DrawableCompat.setTint(vectorDrawable, color);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
     LocationCallback mLocationCallback = new LocationCallback() {
         @Override
@@ -114,13 +167,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             markerOptions.title("You are here");
             markerOptions.icon(BitmapDescriptorFactory.defaultMarker());
             mCurrLocationMarker = mMap.addMarker(markerOptions);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
         }
     };
     public static final int MY_PERMISSION_REQUEST_LOCATION = 99;
     private void checkLocationPermission() {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
+            Log.d("MapsActivity:", "LOCATION PERMISSION NOT GRANTED");
             if(ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
                 new AlertDialog.Builder(this)
@@ -143,10 +197,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         MY_PERMISSION_REQUEST_LOCATION);
             }
         }
+        else {
+            Log.d("MapsActivity:", "LOCATION PERMISSION GRANTED");
+        }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,String permission[],int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode,String[] permission,int[] grantResults) {
         switch(requestCode) {
             case MY_PERMISSION_REQUEST_LOCATION: {
                 if(grantResults.length > 0
